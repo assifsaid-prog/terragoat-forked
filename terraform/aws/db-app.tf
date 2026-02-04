a7ae994e61fa924ba52fc88569725d0a4e18965e
Assif <said@MacBook-Pro-de-Assif.local>
2026-02-04 17:40:58 +0100

    fix: test

--- terraform/aws/db-app.tf
+++ terraform/aws/db-app.tf
@@ -16,10 +16,10 @@ resource "aws_db_instance" "default" {
   apply_immediately       = true
   multi_az                = false
   backup_retention_period = 10
-  storage_encrypted       = false
+  storage_encrypted       = true
   skip_final_snapshot     = true
-  monitoring_interval     = 0
-  publicly_accessible     = true
+  monitoring_interval     = 20
+  publicly_accessible     = false
 
   tags = merge({
     Name        = "${local.resource_prefix.value}-rds"
