bd6c2464241cfb339e0f21fd2d4b6bb26c3cc665
Assif <said@MacBook-Pro-de-Assif.local>
2026-02-06 10:50:54 +0100

    test

--- terraform/gcp/gcs.tf
+++ terraform/gcp/gcs.tf
@@ -19,3 +19,9 @@ resource "google_storage_bucket_iam_binding" "allow_public_read" {
   members = ["allUsers"]
   role    = "roles/storage.objectViewer"
 }
+
+resource "google_storage_bucket_iam_binding2" "allow_public_read" {
+  bucket  = google_storage_bucket.terragoat_website.id
+  members = ["allUsers"]
+  role    = "roles/storage.objectViewer"
+}
