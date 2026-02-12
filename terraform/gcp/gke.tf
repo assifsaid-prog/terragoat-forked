bb9f51283fee65a110f4dbc67042790994364738
Assif <said@MacBook-Pro-de-Assif.local>
2026-02-12 11:20:39 +0100

    test

--- terraform/gcp/gke.tf
+++ terraform/gcp/gke.tf
@@ -89,8 +89,17 @@ resource "google_container_node_pool" "custom_node_pool7" {
   }
 }
 
-
 resource "google_container_node_pool" "custom_node_pool8" {
+  cluster  = google_container_cluster.workload_cluster.name
+  location = var.region
+
+  # nosymbiotic: TF-0331 -- please specify an ignore reason
+  node_config {
+    image_type = "Ubuntu"
+  }
+}
+
+resource "google_container_node_pool" "custom_node_pool9" {
   cluster  = google_container_cluster.workload_cluster.name
   location = var.region
 
